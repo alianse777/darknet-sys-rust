062cf5d3987b121393eb86584877cda81aaaa5ff
jerry73204 <jerry73204@gmail.com>
2020-05-07 11:19:34 +0800

    Implement both building from source and runtime linking

--- build.rs
+++ build.rs
@@ -1,68 +1,89 @@
-use bindgen;
-use cc;
-use copy_dir;
-use std::env;
-use std::fs;
-use std::path::{Path, PathBuf};
-use std::process::Command;
+use failure::{format_err, Fallible};
+use std::{
+    env,
+    path::{Path, PathBuf},
+};
 
-fn obtain_repo() -> PathBuf {
-    let repo_dir = Path::new(&env::var_os("CARGO_MANIFEST_DIR").unwrap()).join("darknet");
-    let out_dir = Path::new(&env::var_os("OUT_DIR").unwrap()).join("darknet");
-    println!(
-        "cargo:rerun-if-changed={}",
-        repo_dir.join("include").join("darknet.h").to_str().unwrap()
-    );
-    println!(
-        "cargo:rerun-if-changed={}",
-        repo_dir.join("Makefile").to_str().unwrap()
-    );
-    println!("Output dir: {}", out_dir.to_str().unwrap());
-    println!("Repo dir: {}", repo_dir.to_str().unwrap());
-    if out_dir.exists() {
-        fs::remove_dir_all(&out_dir);
-    }
-    copy_dir::copy_dir(&repo_dir.to_str().unwrap(), &out_dir).unwrap();
-    out_dir.to_owned()
+const DARKNET_SRC_ENV: &'static str = "DARKNET_SRC";
+const DARKNET_INCLUDE_PATH_ENV: &'static str = "DARKNET_INCLUDE_PATH";
+
+lazy_static::lazy_static! {
+    static ref BINDINGS_TARGET_PATH: PathBuf = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("src").join("bindings.rs");
 }
 
-fn build_with_make(path: &PathBuf) {
-    let _cc = cc::Build::new().get_compiler();
-    let make_status = Command::new("make")
-        .env("CC", _cc.path().to_str().unwrap())
-        .current_dir(&path)
-        .status()
-        .expect("Failed to execute make!");
-    if make_status.success() {
-        println!("cargo:rustc-link-search={}", path.to_str().unwrap());
-        println!("cargo:rustc-link-lib=static=darknet");
-    } else {
-        panic!("make retruned non-zero status!");
-    }
-}
-
-fn gen_bindings(path: &PathBuf) {
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
-    let bindings_path = out_path.join("bindings.rs");
-    println!("Writing bindings to: {}", bindings_path.to_str().unwrap());
+fn gen_bindings<P>(include_path: P) -> Fallible<()>
+where
+    P: AsRef<Path>,
+{
     bindgen::Builder::default()
-        // The input header we would like to generate
-        // bindings for.
-        .header(path.join("include").join("darknet.h").to_str().unwrap())
-        // Tell cargo to invalidate the built crate whenever any of the
-        // included header files changed.
-        //.parse_callbacks(Box::new(bindgen::CargoCallbacks))
-        // Finish the builder and generate the bindings.
+        .header(
+            include_path
+                .as_ref()
+                .join("darknet.h")
+                .to_str()
+                .ok_or_else(|| format_err!("cannot create path to darknet.h"))?,
+        )
         .generate()
-        // Unwrap the Result and panic on failure.
-        .expect("Unable to generate bindings")
-        //.write_to_file(out_path.join("bindings.rs"))
-        .write_to_file(bindings_path)
-        .expect("Couldn't write bindings!");
+        .map_err(|_| format_err!("failed to generate bindings"))?
+        .write_to_file(&*BINDINGS_TARGET_PATH)?;
+    Ok(())
 }
 
-fn main() {
-    let path = obtain_repo();
-    build_with_make(&path);
-    gen_bindings(&path);
+fn build_with_cmake<P>(path: P) -> Fallible<()>
+where
+    P: AsRef<Path>,
+{
+    let path = path.as_ref();
+    let dst = cmake::Config::new(path).build();
+    println!(
+        "cargo:rustc-link-search=native={}",
+        dst.join("build").display()
+    );
+    println!("cargo:rustc-link-lib=darkd");
+
+    gen_bindings(path.join("include"))?;
+
+    Ok(())
+}
+
+fn build_runtime() -> Fallible<()> {
+    if cfg!(feature = "buildtime-bindgen") {
+        let include_path = env::var_os(DARKNET_INCLUDE_PATH_ENV)
+            .map(|value| PathBuf::from(value))
+            .unwrap_or_else(|| {
+                PathBuf::from(env!("CARGO_MANIFEST_DIR"))
+                    .join("darknet")
+                    .join("include")
+            });
+        gen_bindings(include_path)?;
+    }
+
+    Ok(())
+}
+
+fn build_from_source() -> Fallible<()> {
+    let src_dir: PathBuf = match env::var_os(DARKNET_SRC_ENV) {
+        Some(src) => src.into(),
+        None => PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("darknet"),
+    };
+    build_with_cmake(src_dir)?;
+
+    Ok(())
+}
+
+fn main() -> Fallible<()> {
+    println!("cargo:rerun-if-env-changed={}", DARKNET_SRC_ENV);
+    println!("cargo:rerun-if-env-changed={}", DARKNET_INCLUDE_PATH_ENV);
+    println!(
+        "cargo:rerun-if-env-changed={}",
+        BINDINGS_TARGET_PATH.display()
+    );
+
+    // build from source by default
+    if cfg!(feature = "runtime") {
+        build_runtime()?;
+    } else {
+        build_from_source()?;
+    }
+    Ok(())
 }
